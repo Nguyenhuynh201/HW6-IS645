f444df3d205b121b620a40c22f2490852277ab2c
Nguyenhuynh201 <70636146+Nguyenhuynh201@users.noreply.github.com>
2021-03-02 22:42:30 -0800

    Update ex4.js

--- js/ex4.js
+++ js/ex4.js
@@ -1,7 +1,19 @@
 let numtable = document.createElement("TABLE")
-let row = numtable.insertRow(0)
-let newtext = document.createTextNode("1")
-document.getElementsByTagName("div").row.appendChild(newText);
 
 
 
+for (i = 1; i <= 12; i++){
+    let tdElement = document.createElement("td")
+    let trElement = document.createElement("tr")
+    tdElement.textContent = i
+
+    tdElement.style.border = "solid black"
+    if(i % 4 === 0){
+        tdElement.style.backgroundColor ="lightblue"
+    }
+
+    numtable.appendChild(tdElement)
+    numtable.appendChild(trElement)
+}
+
+document.getElementById("output").appendChild(numtable).style.border = "solid black"
